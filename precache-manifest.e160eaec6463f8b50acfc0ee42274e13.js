1d4e6e2f9f07da6b936c600e3613b01b1af7530c
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-06-06 17:53:16 -0700

    Updates

---
+++ precache-manifest.e160eaec6463f8b50acfc0ee42274e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf91ae4381fe2a2502751ae3d783047",
+    "url": "/simpsons-game/index.html"
+  },
+  {
+    "revision": "c4b56fc6fa8f7935a7fe",
+    "url": "/simpsons-game/static/css/main.5c18562f.chunk.css"
+  },
+  {
+    "revision": "c7b8be0303a2da930d38",
+    "url": "/simpsons-game/static/js/2.f5ae0110.chunk.js"
+  },
+  {
+    "revision": "c4b56fc6fa8f7935a7fe",
+    "url": "/simpsons-game/static/js/main.5af4a244.chunk.js"
+  },
+  {
+    "revision": "a2cb7fa87bb7eb0441ba",
+    "url": "/simpsons-game/static/js/runtime~main.19df999e.js"
+  }
+]);
