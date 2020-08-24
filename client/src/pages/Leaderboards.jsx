20bd95b3012611c757e41b57a3133c68e14cba04
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-08-24 14:57:54 -0700

    added filter word state and changed scorepost animations to horizontal

--- client/src/pages/Leaderboards.jsx
+++ client/src/pages/Leaderboards.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/api";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import Scorepost from "../components/Scorepost";
 import Container from "../components/Container";
 import Loading from "../components/Loading";
@@ -7,6 +13,10 @@ import styles from "../styles/Leaderboards.module.scss";
 
 const Leaderboards = () => {
   const [userScores, setScores] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [filter, setFilter] = useState(null);
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   useEffect(() => {
     const scores = API.fetchScores();
     scores
@@ -15,27 +25,83 @@ const Leaderboards = () => {
         throw err;
       });
   }, []);
-  console.log(userScores);
+
+  const sortHighToLow = () => {
+    const newScores = [...userScores];
+    newScores.sort((a, b) => b.Score - a.Score);
+    setScores(newScores);
+    setFilter("Scores - High To Low");
+  };
+
+  const sortLowToHigh = () => {
+    const newScores = [...userScores];
+    newScores.sort((a, b) => a.Score - b.Score);
+    setScores(newScores);
+    setFilter("Scores - Low To High");
+  };
+
+  const sortAlphabetical = () => {
+    const newScores = [...userScores];
+    newScores.sort((a, b) => a.Name.localeCompare(b.Name));
+    setScores(newScores);
+    setFilter("Names - Alphabetical");
+  };
+
+  const sortReverseAlphabetical = () => {
+    const newScores = [...userScores];
+    newScores.sort((a, b) => b.Name.localeCompare(a.Name));
+    setScores(newScores);
+    setFilter("Names - Reverse Alphabetical");
+  };
 
   if (userScores.length === 0) return <Loading />;
   else {
     return (
       <Container className={`${styles["leaderboardContainer"]}`}>
-        <h3
-          style={{ textAlign: "center" }}
-          className={`${styles["leaderboardTitle"]}`}
-        >
-          LEADERBOARDS
-        </h3>
-        {userScores.map((entry) => {
-          return (
-            <Scorepost
-              name={entry.Name}
-              score={entry.Score}
-              key={entry.UserScoreId}
-            />
-          );
-        })}
+        <div className={`${styles["leaderboardMenuAndTitle"]}`}>
+          <h3
+            style={{ textAlign: "center" }}
+            className={`${styles["leaderboardTitle"]}`}
+          >
+            LEADERBOARDS
+          </h3>
+          <Dropdown
+            isOpen={dropdownOpen}
+            toggle={toggle}
+            className={`${styles["dropdownMenu"]}`}
+          >
+            <DropdownToggle caret>Filter</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={() => sortHighToLow()}>
+                Highest to Lowest
+              </DropdownItem>
+              <DropdownItem onClick={() => sortLowToHigh()}>
+                Lowest to Highest
+              </DropdownItem>
+              <DropdownItem onClick={() => sortAlphabetical()}>
+                Alphabetical
+              </DropdownItem>
+              <DropdownItem onClick={() => sortReverseAlphabetical()}>
+                Reverse Alphabetical
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+        <h5 className={`${styles["filter"]}`}>{filter}</h5>
+        <div className={styles["scorePostContainer"]}>
+          {userScores.map((entry) => {
+            return (
+              <div style={{ position: "relative" }}>
+                <Scorepost
+                  className={styles["fade"]}
+                  name={entry.Name}
+                  score={entry.Score}
+                  key={entry.UserScoreId}
+                />
+              </div>
+            );
+          })}
+        </div>
       </Container>
     );
   }
